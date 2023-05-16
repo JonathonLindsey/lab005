3db80c394ce5911d1c7f526fb8e5c8c3a8ef3d41
John Doe <johndoe@example.com01>
2023-05-15 22:28:26 -0500

    update with mongo

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const todoListRouter = require ('./routes/todo.routes')
-const colorMiddleware = require ('./middleware/supportedColors');
+const colorToHexMiddleware = require ('./middleware/supportedColors');
 const errorHandlerMiddleware = require('./middleware/errorHandler');
+const db = require('./lib/database');
+
 
 const port = 4000;
 const app = express();
 
+
 app.use(bodyParser.json());
-app.use(colorMiddleware());
-app.post('*', colorMiddleware());
-app.put('*', colorMiddleware());
-app.patch('*', colorMiddleware());
+app.use(colorToHexMiddleware());
+app.post('*', colorToHexMiddleware());
+app.put('*', colorToHexMiddleware());
+app.patch('*', colorToHexMiddleware());
 
 app.use('/api/v1/todoList', todoListRouter);
 
 app.use(errorHandlerMiddleware());
+
+const dbConfig = {
+    hostname: '127.0.0.1',
+    port: 27017,
+    database: 'arca',
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      maxPoolSize: 3,
+    }
+  };
+  
+  db.configure(dbConfig);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
